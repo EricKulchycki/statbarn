f61c2529a518e9aeaeb391ae5d1255a0742053b3
Eric Kulchycki <erickulchycki@gmail.com>
2025-08-25 20:36:07 -0500

    placeholder page

--- src/app/predictions/[team]/page.tsx
+++ src/app/predictions/[team]/page.tsx
@@ -1,15 +1,14 @@
-'use client'
-
-import { useRouter } from 'next/router'
-
-export default function Team() {
-  const router = useRouter()
-
+export default async function Team({
+  params,
+}: {
+  params: Promise<{ team: string }>
+}) {
+  const { team } = await params
   return (
     <div className="flex justify-center">
       <div>
         <h1 className="text-2xl font-bold mb-6">
-          Team Predictions Page for {router.query.team}
+          Team Predictions Page for {team}
         </h1>
         <p>This is a placeholder for the team predictions page.</p>
       </div>
